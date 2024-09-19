7696f394ff4f0ddadc928648ad972c00917a876b
PGrytsiuk <pgrytsiuk1992@gmail.com>
2024-09-20 02:00:05 +0300

    Commit test updates

--- src/main/java/PlayWrightTests2/LunchingBrowser.java
+++ src/main/java/PlayWrightTests2/LunchingBrowser.java
@@ -2,8 +2,11 @@ package PlayWrightTests2;
 
 import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.BrowserType.LaunchOptions;
+import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
 public class LunchingBrowser {
 
@@ -15,6 +18,18 @@ public class LunchingBrowser {
         );
         Page page = browser.newPage();
         page.navigate("https://gym.langfit.net/login");
+
+        Locator username = page.locator("[name='username']");
+        username.fill("pavlo_grytsiuk");
+        Locator password = page.locator("[name='userPassword']");
+        password.fill("pgry2412");
+        Locator SignIn =page.locator("[aria-label='sign in']");
+        SignIn.click();
+        Locator Host = page.locator("//*[contains (@class, 'user-name')]");
+       /* Host.getByText("Pavlo Grytsiuk");*/
+        assertThat(Host).hasText("Pavlo Grytsiuk");
+        browser.close();
+
 
     }
 
