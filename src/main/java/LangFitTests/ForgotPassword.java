81fd20d2ef88dd2d6ceb9efbfbc67c7eb3303ec5
PGrytsiuk <33345335+PGrytsiuk@users.noreply.github.com>
2024-10-08 21:35:35 +0300

    Forgot password test (#1)
    * ForgotPassword test
    
    * ForgotPassword test
    
    * ForgotPassword test fixes

--- src/main/java/LangFitTests/ForgotPassword.java
+++
@@ -1,4 +0,0 @@
-package LangFitTests;
-
-public class ForgotPassword {
-}
--- src/main/java/LangFitTests/TestNGtest.java
+++ src/main/java/LangFitTests/TestNGtest.java
@@ -3,13 +3,23 @@ package LangFitTests;
 import Pages.LoginPage;
 import Utils.ScreenshotsAndRecordings;
 import com.microsoft.playwright.*;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 
 public class TestNGtest {
 
-    @Test(priority = 3, enabled = false)
-    public void InvalidLogin(){
+    @DataProvider(name="Invalid users")
+    public Object[][] InvalidUsers() {
+
+
+        return new Object[][]{{"1pavlo_grytsiuk1233", "pgry2412123"},
+                {"2pavlo_grytsiuk1235", "pgry24121223"},
+                {"3pavlo_grytsiuk1247", "pgry2412143"}};
+    }
+
+    @Test(priority = 3, dataProvider = "Invalid users")
+    public void InvalidLogin(String login, String password){
         Playwright playwright = Playwright.create();
         Browser browser = playwright.chromium().launch(
                 new BrowserType.LaunchOptions().setHeadless(false)
@@ -20,7 +30,7 @@ public class TestNGtest {
 
             LoginPage loginPage = new LoginPage(page);
 
-            loginPage.login("pavlo_grytsiuk1233", "pgry2412123");
+            loginPage.login(login, password);
             // Verify the alert
             boolean Toast = loginPage.AlertAppear();
             loginPage.getToastMessageText("Invalid username or password");
