96264279c45d141aacfe63d10281d041d2184daf
Pavlo <33345335+PGrytsiuk@users.noreply.github.com>
2024-10-27 18:49:27 +0200

    Refactoring Allure report v3 (#64)

--- src/main/java/Hooks/Setup.java
+++ src/main/java/Hooks/Setup.java
@@ -4,51 +4,88 @@ import com.microsoft.playwright.*;
 import io.qameta.allure.Attachment;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+
 import java.nio.file.Paths;
-
-
 
 public class Setup {
 
-    protected Playwright playwright;
-    protected Browser browser;
-    protected BrowserContext context;
-    protected Page page;
+    protected static Playwright playwright;
+    protected static Browser browser;
+    protected static BrowserContext context;
+    protected static Page page;
 
-    @BeforeMethod
+    @BeforeSuite
     public void setUp() {
-        playwright = Playwright.create();
-        browser = playwright.chromium().launch(
-                new BrowserType.LaunchOptions().setHeadless(true)
-        );
-
-
-        this.page = browser.newPage();
-
-        this.context = browser.newContext();
+        try {
+            playwright = Playwright.create();
+            browser = playwright.chromium().launch(
+                    new BrowserType.LaunchOptions().setHeadless(true)
+            );
+        } catch (Exception e) {
+            e.printStackTrace(); // This will help you see any exceptions thrown on the console.
+        }
     }
 
+    @BeforeMethod
+    public void createContextAndPage() {
+        context = browser.newContext();
+        page = context.newPage(); // Initialize the page
+    }
 
     @AfterMethod
-    public void tearDown(ITestResult result) {
-        if (page != null && result.getStatus() == ITestResult.FAILURE) {
-            // Take screenshot only if the test fails
-            byte[] screenshotBytes = page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("target/screenshots", result.getName() + ".png")).setFullPage(true));
-            // Attach the screenshot to the Allure report
-            saveScreenshot(screenshotBytes, result.getName());
-
+    public void closeContextAndAddScreenshotIfFail(ITestResult result) {
+        if (result.getStatus() == ITestResult.FAILURE) {
+            takeScreenshotForPage(page, result.getName());
         }
 
-        if (page != null) page.close();
+        // Close all pages in the context
+        for (Page page : context.pages()) {
+            if (page != null) {
+                page.close();
+            }
+        }
+
+        // Then close the context
         if (context != null) context.close();
-        if (browser != null) browser.close();
-        if (playwright != null) playwright.close();
+    }
+
+    private void takeScreenshotForPage(Page page, String testName) {
+        if (page != null) {
+            byte[] screenshotBytes = page.screenshot(new Page.ScreenshotOptions()
+                    .setPath(Paths.get("target/screenshots", testName + ".png"))
+                    .setFullPage(true)
+            );
+            // Attach the screenshot to the Allure report
+            saveScreenshot(screenshotBytes, testName);
+        }
     }
 
     @Attachment(value = "Screenshot of {testName}", type = "image/png")
-    public byte[] saveScreenshot(byte[] screenShot, String testName) {
-        return screenShot;
+    public byte[] saveScreenshot(byte[] screenshot, String testName) {
+        return screenshot;
     }
 
+    @AfterSuite(alwaysRun = true)
+    public void tearDown() {
+        // Close all contexts and their pages
+        if (browser != null) {
+            for (BrowserContext ctx : browser.contexts()) {
+                for (Page p : ctx.pages()) {
+                    if (p != null) {
+                        p.close();
+                    }
+                }
+                ctx.close();
+            }
+
+            browser.close();
+        }
+
+        if (playwright != null) {
+            playwright.close();
+        }
+    }
 }
