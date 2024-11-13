ebaf0bdf8900dd4140890d7b93f2bd1f6c4e561d
PGrytsiuk <pgrytsiuk1992@gmail.com>
2024-11-13 18:41:57 +0200

    API tests v4

---
+++ src/main/java/APITests/RouteFulFillDemo.java
@@ -0,0 +1,41 @@
+package APITests;
+
+import com.microsoft.playwright.*;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RouteFulFillDemo {
+
+    Playwright pw;
+    Browser browser;
+    String token ="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";
+
+    @Test
+    public void requestChangeDemo() {
+
+        pw = Playwright.create();
+        browser = pw.chromium().launch(new BrowserType.LaunchOptions().setHeadless(true));
+        BrowserContext ctx = browser.newContext(new Browser.NewContextOptions()
+                .setExtraHTTPHeaders(Map.of("Authorization", "token " + token))
+        );
+
+        Page page = ctx.newPage();
+
+        page.route("**/*", route -> {
+            Map<String, String> headers = new HashMap<>(route.request().headers());
+            headers.put("Test", "added header");
+            headers.remove("authorization");
+            route.resume(new Route.ResumeOptions().setHeaders(headers));
+         });
+
+        Response response = page.navigate("https://api.github.com/user");
+        System.out.println(response.request().headers());
+        System.out.println(response.text());
+        Assert.assertEquals(401, response.status());
+       /* Assert.assertEquals(200, response.status());*/
+    }
+
+}
